b5abe3f5e63edb5afeea79b4988861011b534838
wsfreund <wsfreund@gmail.com>
2009-08-26 11:49:25 -0300

    removido comentarios e pronto para rodar a carga total

--- rootSeq.cxx
+++ rootSeq.cxx
@@ -7,64 +7,50 @@ RootSeq::RootSeq(TChain *outsideReadingChain, TTree *outsidefillingTree){
 
 //NeuralRinger Variables
     readingChain->SetBranchStatus("Ringer_Rings", 		true);
-//	readingChain->SetBranchStatus("Ringer_LVL2_Eta", 	true);
-//	readingChain->SetBranchStatus("Ringer_LVL2_Phi",    true);
-//	readingChain->SetBranchStatus("Ringer_LVL2_Et",		true);
+	readingChain->SetBranchStatus("Ringer_LVL2_Eta", 	true);
+	readingChain->SetBranchStatus("Ringer_LVL2_Phi",    true);
+	readingChain->SetBranchStatus("Ringer_LVL2_Et",		true);
 
 //T2Calo Variables
-//	readingChain->SetBranchStatus("T2CaEta", 		true);
-//	readingChain->SetBranchStatus("T2CaPhi",		true);
-//	readingChain->SetBranchStatus("T2CaRcore", 	    true);
-//	readingChain->SetBranchStatus("T2CaEratio",	    true);
-//	readingChain->SetBranchStatus("T2CaEmES1", 	    true);
-//	readingChain->SetBranchStatus("T2CaEmE", 		true);
-//	readingChain->SetBranchStatus("T2CaHadES0", 	true);
+	readingChain->SetBranchStatus("T2CaEta", 		true);
+	readingChain->SetBranchStatus("T2CaPhi",		true);
+	readingChain->SetBranchStatus("T2CaRcore", 	    true);
+	readingChain->SetBranchStatus("T2CaEratio",	    true);
+	readingChain->SetBranchStatus("T2CaEmES1", 	    true);
+	readingChain->SetBranchStatus("T2CaEmE", 		true);
+	readingChain->SetBranchStatus("T2CaHadES0", 	true);
 
 //NeuralRinger Variables
     readingChain->SetBranchAddress("Ringer_Rings",      &ringer_rings);
-//    readingChain->SetBranchAddress("Ringer_LVL2_Eta",   &ringer_lvl2_eta);       
-//    readingChain->SetBranchAddress("Ringer_LVL2_Phi",   &ringer_lvl2_phi);       
-//   readingChain->SetBranchAddress("Ringer_LVL2_Et",	&ringer_lvl2_et);
+    readingChain->SetBranchAddress("Ringer_LVL2_Eta",   &ringer_lvl2_eta);       
+    readingChain->SetBranchAddress("Ringer_LVL2_Phi",   &ringer_lvl2_phi);       
+    readingChain->SetBranchAddress("Ringer_LVL2_Et",	&ringer_lvl2_et);
 
 //T2Calo Variables
-//	readingChain->SetBranchAddress("T2CaEta", 		&t2ca_lvl2_eta);
-//	readingChain->SetBranchAddress("T2CaPhi",		&t2ca_lvl2_phi);
-//	readingChain->SetBranchAddress("T2CaRcore", 	&t2ca_rcore);
-//	readingChain->SetBranchAddress("T2CaEratio",	&t2ca_eratio);
-//	readingChain->SetBranchAddress("T2CaEmES1", 	&t2ca_emes1);
-//	readingChain->SetBranchAddress("T2CaEmE", 		&t2ca_eme);
-//	readingChain->SetBranchAddress("T2CaHadES0", 	&t2ca_ehades0);
-
+	readingChain->SetBranchAddress("T2CaEta", 		&t2ca_lvl2_eta);
+	readingChain->SetBranchAddress("T2CaPhi",		&t2ca_lvl2_phi);
+	readingChain->SetBranchAddress("T2CaRcore", 	&t2ca_rcore);
+	readingChain->SetBranchAddress("T2CaEratio",	&t2ca_eratio);
+	readingChain->SetBranchAddress("T2CaEmES1", 	&t2ca_emes1);
+	readingChain->SetBranchAddress("T2CaEmE", 		&t2ca_eme);
+	readingChain->SetBranchAddress("T2CaHadES0", 	&t2ca_ehades0);
 //NeuralRinger
     fillingTree->Branch("Ringer_Rings",      &ringer_rings);
-//    fillingTree->Branch("Ringer_LVL2_Eta",   &ringer_lvl2_eta2);       
-//    fillingTree->Branch("Ringer_LVL2_Phi",   &ringer_lvl2_phi2);       
-//   fillingTree->Branch("Ringer_LVL2_Et",	 &ringer_lvl2_et2);
+    fillingTree->Branch("Ringer_LVL2_Eta",   &ringer_lvl2_eta2);       
+    fillingTree->Branch("Ringer_LVL2_Phi",   &ringer_lvl2_phi2);       
+    fillingTree->Branch("Ringer_LVL2_Et",	 &ringer_lvl2_et2);
 
 //T2Calo
-//	fillingTree->Branch("T2CaEta", 		&t2ca_lvl2_eta2);
-//	fillingTree->Branch("T2CaPhi",		&t2ca_lvl2_phi2);
-//	fillingTree->Branch("T2CaRcore", 	&t2ca_rcore2);
-//	fillingTree->Branch("T2CaEratio",	&t2ca_eratio2);
-//	fillingTree->Branch("T2CaEmES1", 	&t2ca_emes12);
-//	fillingTree->Branch("T2CaEmE", 		&t2ca_eme2);
-//	fillingTree->Branch("T2CaHadES0", 	&t2ca_ehades02);
+	fillingTree->Branch("T2CaEta", 		&t2ca_lvl2_eta2);
+	fillingTree->Branch("T2CaPhi",		&t2ca_lvl2_phi2);
+	fillingTree->Branch("T2CaRcore", 	&t2ca_rcore2);
+	fillingTree->Branch("T2CaEratio",	&t2ca_eratio2);
+	fillingTree->Branch("T2CaEmES1", 	&t2ca_emes12);
+	fillingTree->Branch("T2CaEmE", 		&t2ca_eme2);
+	fillingTree->Branch("T2CaHadES0", 	&t2ca_ehades02);
 
 }
 
-/*
-void RootSeq::copyVectors(){
-
-    ringer_rings2->insert(ringer_rings2->begin(), ringer_rings->begin(), ringer_rings->end());
-
-}
-
-void RootSeq::clearVectors(){
-
-    ringer_rings2->clear();
-
-}
-*/
 inline unsigned RootSeq::getLayerInit(const unsigned numEvent, const unsigned curLayer){
     unsigned layerInitialRing=0;
 
