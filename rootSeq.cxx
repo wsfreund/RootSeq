84d922d6bfbf2f54e40dd5b49d896249be35abb5
wsfreund <wsfreund@gmail.com>
2009-08-27 13:06:41 -0300

    Colocado denovo a alocaçao dinamica

--- rootSeq.cxx
+++ rootSeq.cxx
@@ -10,7 +10,7 @@ RootSeq::RootSeq(TChain *outsideReadingChain, TTree *outsidefillingTree)
 	ringer_lvl2_eta	=	new std::vector<float>;
 	ringer_lvl2_phi	=	new std::vector<float>;
 	ringer_lvl2_et  =	new std::vector<float>;
-
+    ringer_nclusters=   new std::vector<float>;
 
 	t2ca_lvl2_eta	=	new std::vector<float>;
 	t2ca_lvl2_phi   =	new std::vector<float>;
@@ -19,6 +19,7 @@ RootSeq::RootSeq(TChain *outsideReadingChain, TTree *outsidefillingTree)
     t2ca_emes1		=	new std::vector<float>;
     t2ca_eme		=	new std::vector<float>;
     t2ca_ehades0	=	new std::vector<float>;
+    t2canclus       =   new std::vector<float>;
     t2cahade        =   new std::vector<float>;
 
 
@@ -254,6 +255,7 @@ RootSeq::~RootSeq(){
 	delete ringer_lvl2_eta;	
 	delete ringer_lvl2_phi;	
 	delete ringer_lvl2_et;  
+    delete ringer_nclusters;
 
 	delete t2ca_lvl2_eta;	
 	delete t2ca_lvl2_phi;   
@@ -262,6 +264,7 @@ RootSeq::~RootSeq(){
     delete t2ca_emes1;		
     delete t2ca_eme;		
     delete t2ca_ehades0;
+    delete t2canclus;
     delete t2cahade;
 	
 }
